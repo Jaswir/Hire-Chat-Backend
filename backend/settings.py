8f7fba4f84cbad59f20e7ebb60b6b135e222c0f2
Jaswir <jaswirraghoe@gmail.com>
2024-05-09 22:14:26 +0200

    Updated hosts file

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-&e(0h#0+ybvan2c1-d+ip3_m5mkh)5q5i787lw1_jo_(45q61z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "hire-chat-backend-500d6356f1a0.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "hire-chat-backend-bd580f5a0fb7.herokuapp.com"]
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = ("http://localhost:5173",)
 
